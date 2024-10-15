b9993426abc559380fe9f04401f37a5e4ced011f
Samed Demiroz <sdemiroz@2-B-7.42heilbronn.de>
2024-10-15 17:51:58 +0200

    15.10

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdemiroz <sdemiroz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 17:23:45 by sdemiroz          #+#    #+#             */
+/*   Updated: 2024/10/15 17:40:55 by sdemiroz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
