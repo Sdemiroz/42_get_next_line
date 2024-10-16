4864ae192f9f83af6e368f9a6385e054dc195391
Samed Demiroz <sdemiroz@2-C-5.42heilbronn.de>
2024-10-16 20:52:11 +0200

    16.10.

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,49 @@
 /*   By: sdemiroz <sdemiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 17:23:42 by sdemiroz          #+#    #+#             */
-/*   Updated: 2024/10/15 17:40:37 by sdemiroz         ###   ########.fr       */
+/*   Updated: 2024/10/16 20:50:57 by sdemiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char	*ft_join(char *temp, char *buffer)
+{
+	char	*result;
+
+	result = ft_strjoin(temp, buffer);
+	free (temp);
+	return (result);
+}
+
+static char	*ft_get_buffer(int fd, char *buffer)
+{
+	char	*temp;
+	size_t	cc;
+
+	cc = 1;
+	if (!buffer)
+		buffer = malloc(1);
+	temp = malloc(BUFFER_SIZE + 1);
+	if (!temp)
+		return (NULL);
+	while (cc > 0)
+	{
+		cc = read(fd, temp, BUFFER_SIZE);
+		if (cc == -1)
+		{
+			free (temp);
+			free (buffer);
+			return (NULL);
+		}
+		temp[cc] = '\0';
+		buffer = ft_join(buffer, temp);
+		if (ft_strnchr(temp, '\n'))
+			break ;
+	}
+	free (temp);
+	return (buffer);
+}
 
 char	*get_next_line(int fd)
 {
@@ -19,4 +57,6 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	buffer = ft_get_buffer(fd, buffer);
+	return (res);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,59 @@
 /*   By: sdemiroz <sdemiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 17:23:39 by sdemiroz          #+#    #+#             */
-/*   Updated: 2024/10/15 17:23:41 by sdemiroz         ###   ########.fr       */
+/*   Updated: 2024/10/16 20:45:56 by sdemiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new;
+	size_t	x;
+	size_t	y;
+	size_t	len;
+
+	x = 0;
+	y = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	new = (char *)malloc(len + 1);
+	if (new == NULL)
+		return (NULL);
+	while (s1[x])
+	{
+		new[x] = s1[x];
+		x++;
+	}
+	while (s2[y])
+	{
+		new[x + y] = s2[y];
+		y++;
+	}
+	new[len] = '\0';
+	return (new);
+}
+
+size_t	ft_strlen(const char *c)
+{
+	int	a;
+
+	a = 0;
+	while (c[a] != '\0')
+		a++;
+	return (a);
+}
 
